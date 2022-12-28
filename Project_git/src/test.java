1c18a8c592d6c9781fe561ebc78d2196557f608c
Pratik <Pratik@gmail.com>
2022-12-28 15:58:11 +0530

    by pratik

--- Project_git/src/test.java
+++ Project_git/src/test.java
@@ -3,6 +3,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("github demo ");
 		System.out.println("pratik");
-		System.out.println("");
+		System.out.println("chunkikar");
 	}
 }
